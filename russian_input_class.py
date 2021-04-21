7f91f57a9f8efd25dcdfb5bc942724d507002384
ShineEzz <helenzapal@mail.ru>
2021-04-21 20:23:12 +0300

    Update russian_input_class.py
    added print in russian

--- russian_input_class.py
+++ russian_input_class.py
@@ -1,24 +1,17 @@
-Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> class Ввод():
-	def __call__(self,*args,**kwargs):
-		return input(*args,**kwargs)
+#!/usr/bin/env
 
-	
->>> ввод_имени = Ввод()
->>> ввод_возраста = Ввод()
->>> имя = ввод_имени("Введите ваше имя: ")
-Введите ваше имя: Helen
->>> 
->>> имя
-'Helen'
->>> возраст = ввод_возраста("Сколько вам лет? )
-			
-SyntaxError: EOL while scanning string literal
->>> возраст = ввод_возраста("Сколько вам лет? ")
-Сколько вам лет? 30
->>> возраст
-'30'
->>> имя
-'Helen'
->>> 
+class Ввод():
+    def __call__(self,*args,**kwargs):
+        return input(*args,**kwargs)
+
+def вывести(*args,**kwargs):
+    return print(*args,**kwargs)
+
+def main():
+     ввод_имени = Ввод()
+     ввод_возраста = Ввод()
+     имя = ввод_имени("Ваше имя? ")
+     лет = ввод_возраста("Сколько вам лет? ")
+     вывести("Привет, {имя}, вам {лет} лет!")
+
+if __name__ == "__main__":main()
